60f5e3292b88f96369347ea112957c3aa5eccbce
Heberth Reinstein <heberth.reinstein@gmail.com>
2021-10-24 16:19:30 +0000

    fix dbname

--- API/mongodb.js
+++ API/mongodb.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb');
 
-const uri = "mongodb+srv://admin:cMySPn7PBImhJYUo@cluster0.jxoyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:<password>@cluster0.jxoyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect()
     .then(() => console.log("Connected to the database"))
     .catch(err => console.log(err));
 
-const collection = client.db("retrospectiveSFA").collection("respostas");
+const collection = client.db("restrospectiveSFA").collection("respostas");
 
 module.exports = {collection};
